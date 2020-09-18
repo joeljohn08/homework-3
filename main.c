33cb29f6c88349de3b645e0abfc6dbb6bdf41b45
JoelJohn08 <jmj6182@psu.edu>
2020-09-18 10:19:32 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int digit_sum(int n);
+
+
+int main()
+{
+    int num, sum;
+    
+    printf("Enter an int: ");
+    scanf("%d", &num);
+    
+    sum = digit_sum(num);
+    
+    printf("Sum of digits of %d is %d", num, sum);
+    
+    return 0;
+}
+
+
+int digit_sum(int n)
+{
+    if(n == 0)
+        return 0;
+        
+    return ((n % 10) + digit_sum(n / 10));
+}
